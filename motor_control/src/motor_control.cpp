14f075b4a12c15c0e26ac78312210b0e44bbe3ef
trystonfreeman <trystonfreeman@gmail.com>
2026-01-20 15:17:40 -0500

    motor control kinda does something now

---
+++ motor_control/src/motor_control.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "interfaces/msg/motordata.hpp"
+#include "interfaces/msg/motorcontrol.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class MotorController : public rclcpp::Node
+{
+public:
+    MotorController()
+    : Node("MotorController"), count_(0)
+    {
+        publisher_ = this->create_publisher<std_msgs::msg::motordata>("motor_outputs", 10);
+        timer_ = this->create_wall_timer(
+        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+private:
+    void timer_callback()
+    {
+        auto message = std_msgs::msg::motordata();
+        message.pos[0] = 0;
+        message.vel[0] = 0;
+        message.torque[0] = 0;
+
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%i'", message.pos[0]);
+        publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalPublisher>());
+    rclcpp::shutdown();
+    return 0;
+}
