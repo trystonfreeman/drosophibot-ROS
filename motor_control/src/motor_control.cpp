5359225e90a42b2f0b5d1fd1fdc660db74b5aa01
trystonfreeman <trystonfreeman@gmail.com>
2026-01-21 15:32:10 -0500

    motor test take 2

--- motor_control/src/motor_control.cpp
+++ motor_control/src/motor_control.cpp
@@ -54,7 +54,7 @@ int main(int argc, char * argv[])
 	portHandler->openPort();
   	portHandler->setBaudRate(1000000);
  	uint8_t dxl_id = 1;
- 	uint16_t torque_on_address = 64;
+ 	//uint16_t torque_on_address = 64;
 	uint16_t LED_address = 65;
  	uint8_t data = 1; // 1 to turn on the torque, 0 to turn off
  	packetHandler->write1ByteTxRx(portHandler, dxl_id, LED_address, data);
