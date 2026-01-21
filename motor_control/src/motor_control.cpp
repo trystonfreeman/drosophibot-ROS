19e669ee01e933d17686a39b60fdbed23203db58
trystonfreeman <trystonfreeman@gmail.com>
2026-01-21 16:28:02 -0500

    start on reading motor commands

--- motor_control/src/motor_control.cpp
+++ motor_control/src/motor_control.cpp
@@ -14,9 +14,10 @@ using std::placeholders::_1;
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 uint8_t dxl_id = 1;
-//uint16_t torque_on_address = 64;
+uint16_t torque_on_address = 64;
 uint16_t LED_address = 65;
 uint16_t present_position_address = 132;
+uint16_t present_velocity_address = 128;
 uint8_t data = 1; // 1 to turn on the torque, 0 to turn off
 
 
@@ -40,8 +41,11 @@ private:
         auto message = interfaces::msg::MotorData();
         for (uint8_t id = 0; id < 22; id++){
 			uint32_t position = 0;
+			uint32_t velocity = 0;
 			packetHandler->read4ByteTxRx(portHandler, id, present_position_address, &position);
+			packetHandler->read4ByteTxRx(portHandler, id, present_velocity_address, &velocity);
 			message.pos[id] = position;
+			message.vel[id] = velocity;
 			RCLCPP_INFO(this->get_logger(), "ID: '%i'", id);
         	RCLCPP_INFO(this->get_logger(), "Position: '%i'", message.pos[id]);
         	publisher_->publish(message);
@@ -64,7 +68,9 @@ int main(int argc, char * argv[])
 	dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(2.0); //protocol version
 	portHandler->openPort();
 	portHandler->setBaudRate(1000000);
- 	packetHandler->write1ByteTxRx(portHandler, dxl_id, LED_address, data);
+ 	for (int id = 0; id < 22; id++){
+		packetHandler->write1ByteTxRx(portHandler, id, torque_on_address, data);
+	}
 
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MotorController>(portHandler,packetHandler));
