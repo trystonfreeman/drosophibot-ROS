319e9ad0b2f3d5da7886c5e39ab525c597639311
trystonfreeman <trystonfreeman@gmail.com>
2026-01-21 13:16:54 -0500

    dynamixel maybe?

---
+++ build/motor_control/CMakeFiles/motor_control.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/motor_control.dir/src/motor_control.cpp.o"
+  "CMakeFiles/motor_control.dir/src/motor_control.cpp.o.d"
+  "motor_control"
+  "motor_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motor_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
