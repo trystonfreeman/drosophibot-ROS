dfaebf0e102da9ef88f5bca117c809fa43cecde9
trystonfreeman <trystonfreeman@gmail.com>
2026-01-22 14:46:38 -0500

    limb controlla baby

---
+++ limb_controller/src/limb_controller.cpp
@@ -0,0 +1,37 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+class LimbController : public rclcpp::Node
+{
+public:
+    LimbController()
+    : Node("LimbController"), count_(0)
+    {
+        publisher_ = this->create_publisher<interfaces::msg::LimbCommand>("LimbCommands", 10);
+        timer_ = this->create_wall_timer(
+        500ms, std::bind(&LimbController::timer_callback, this));
+    }
+
+private:
+    void timer_callback()
+    {
+        auto message = interfaces::msg::LimbCommand();
+
+        publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<interfaces::msg::LimbCommand>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<LimbController>());
+    rclcpp::shutdown();
+    return 0;
+}
